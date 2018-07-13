c2f95384efe33cce6aa1fbf8e1b11ba2d6784c4a
aosa <shoara0509@gmail.com>
2018-07-14 00:20:27 +0900

    add backdrop component
    add backdrop component to whiskeys/scotch

--- app/src/main/java/io/github/aosa4054/whiskeynote/whiskeys/scotch/ScotchesFragment.kt
+++ app/src/main/java/io/github/aosa4054/whiskeynote/whiskeys/scotch/ScotchesFragment.kt
@@ -1,30 +1,60 @@
 package io.github.aosa4054.whiskeynote.whiskeys.scotch
 
+import android.os.Build
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.GridLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AccelerateDecelerateInterpolator
+import io.github.aosa4054.whiskeynote.R
 import io.github.aosa4054.whiskeynote.databinding.ScotchesListItemBinding
+import kotlinx.android.synthetic.main.activity_scotches.*
+import kotlinx.android.synthetic.main.fragment_scotches.view.*
 
 class ScotchesFragment: Fragment(){
     private lateinit var mViewModel: ScotchesViewModel
     private lateinit var mListItemBinding: ScotchesListItemBinding
     private lateinit var mhRecyclerViewAdapter: ScotchRecyclerViewAdapter
 
-    companion object {
-        fun newInstance(): ScotchesFragment{return ScotchesFragment()}
-    }
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+
+        /*
+        //要チェック
         mListItemBinding = ScotchesListItemBinding.inflate(inflater, container, false)
         mListItemBinding.viewModel = mViewModel
         val root: View = mListItemBinding.root
         return root
+        */
+
+        /*
+        以下はonActivityCreatedに記述？
+        TODO: setup toolbar
+        TODO: setup fab
+        TODO: setup RecyclerView (maintain related classes like Adapter)
+        TODO: inflate -> return
+         */
+        
+        val view = inflater.inflate(R.layout.fragment_scotches, container, false)
+
+        // Set up the tool bar
+        (activity as AppCompatActivity).setSupportActionBar(view.app_bar)
+        view.app_bar.setNavigationOnClickListener(NavigationIconClickListener(activity!!, view.product_grid, AccelerateDecelerateInterpolator()))
+
+        // Set up the RecyclerView
+        
+        // Set cut corner background for API 23+
+
+        return view;
+
     }
 
     fun setViewModel(viewModel: ScotchesViewModel){
         mViewModel = viewModel
     }
+
+
 }
